7a7abe34b30c809d906b65e256df466f37df3096
redpenguinyt <redpenguin777@yahoo.com>
2024-03-22 03:00:21 +0000

    delete old zip file before zipping

--- build_for_windows.sh
+++ build_for_windows.sh
@@ -6,4 +6,5 @@ cp assets orbits_editor_win/ -r
 cp target/x86_64-pc-windows-gnu/release/orbits_editor.exe orbits_editor_win/
 
 echo "Zipping up"
+rm orbits_editor.zip
 zip orbits_editor.zip orbits_editor_win -r
