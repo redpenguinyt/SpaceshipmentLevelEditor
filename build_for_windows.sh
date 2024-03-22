d4271177ec9aca5ea70b034bf6db550115e23dd8
redpenguinyt <redpenguin777@yahoo.com>
2024-03-22 02:56:00 +0000

    zip up archive with build_for_windows.sh

--- build_for_windows.sh
+++ build_for_windows.sh
@@ -1,6 +1,9 @@
 echo "Building"
 cargo build --release --target x86_64-pc-windows-gnu
 
-echo "Copying Files"
+echo "Copying files"
 cp assets orbits_editor_win/ -r
 cp target/x86_64-pc-windows-gnu/release/orbits_editor.exe orbits_editor_win/
+
+echo "Zipping up"
+zip orbits_editor.zip orbits_editor_win -r
