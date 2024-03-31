42d674683aced0d893ca475a00505844c36624ac
redpenguinyt <redpenguin777@yahoo.com>
2024-03-31 17:37:24 +0100

    fmt

--- src/context/selection.rs
+++ src/context/selection.rs
@@ -20,16 +20,22 @@ impl Selection {
         }
     }
 
-	/// Attempt to select a body. Returns true if the body is actually selected
-    pub fn try_select(&mut self, mouse_pos: Vec2F, body: SelectedBody, body_pos: Vec2F, body_radius: f64) -> bool {
+    /// Attempt to select a body. Returns true if the body is actually selected
+    pub fn try_select(
+        &mut self,
+        mouse_pos: Vec2F,
+        body: SelectedBody,
+        body_pos: Vec2F,
+        body_radius: f64,
+    ) -> bool {
         let distance_to_body = (body_pos - mouse_pos).magnitude();
 
-		let is_selectable = distance_to_body < body_radius;
+        let is_selectable = distance_to_body < body_radius;
         if is_selectable {
             self.body = body;
             self.last_mouse_position = mouse_pos;
         }
 
-		is_selectable
+        is_selectable
     }
 }
