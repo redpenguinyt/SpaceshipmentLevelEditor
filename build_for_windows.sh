642610d1610238c6b51d2d546418831e7d85f284
redpenguinyt <redpenguin777@yahoo.com>
2024-04-01 19:48:49 +0100

    updated build script for new levels location

--- build_for_windows.sh
+++ build_for_windows.sh
@@ -2,7 +2,7 @@ echo "Building"
 cargo build --release --target x86_64-pc-windows-gnu
 
 echo "Copying files"
-cp assets orbits_editor_win/ -r
+cp levels orbits_editor_win/ -r
 cp target/x86_64-pc-windows-gnu/release/orbits_editor.exe orbits_editor_win/
 
 echo "Zipping up"
