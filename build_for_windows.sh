789ae001de6116009b7ed9aa2add1c7b53dcc495
redpenguinyt <redpenguin777@yahoo.com>
2024-04-02 22:57:56 +0100

    delete old level files from windows build

--- build_for_windows.sh
+++ build_for_windows.sh
@@ -2,6 +2,7 @@ echo "Building"
 cargo build --release --target x86_64-pc-windows-gnu
 
 echo "Copying files"
+rm orbits_editor_win/levels/ -r
 cp levels orbits_editor_win/ -r
 cp target/x86_64-pc-windows-gnu/release/orbits_editor.exe orbits_editor_win/
 
